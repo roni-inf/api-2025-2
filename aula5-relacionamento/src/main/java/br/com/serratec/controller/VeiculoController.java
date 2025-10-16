43183a06553828923663312189b720bd67220b55
roni_inf <roni_inf@hotmail.com>
2025-10-16 13:59:31 -0300

    Aula 6 - Swagger e Herança

--- aula5-relacionamento/src/main/java/br/com/serratec/controller/VeiculoController.java
+++ aula5-relacionamento/src/main/java/br/com/serratec/controller/VeiculoController.java
@@ -13,25 +13,46 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.serratec.entity.Veiculo;
 import br.com.serratec.repository.VeiculoRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
+@Tag(name = "Veículo", description = "Cadastro de Veículos")
 @RestController
 @RequestMapping("/veiculos")
 public class VeiculoController {
 	@Autowired
 	private VeiculoRepository repository;
+
 	
+	@Operation(summary = "Cadastra um veiculo", description = "A resposta é o veículos salvo ")
+	@ApiResponses(value = { @ApiResponse(responseCode = "201", content = {
+			@Content(schema = @Schema(implementation = Veiculo.class), mediaType = "application/json") }, description = "Veículo cadastrado"),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso"),
+			@ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
+			@ApiResponse(responseCode = "505", description = "Exceção interna da aplicação") })
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Veiculo inserir(@Valid @RequestBody Veiculo veiculo) {
 		return repository.save(veiculo);
 	}
-	
+
+
+	@Operation(summary = "Lista todos os veiculos", description = "A resposta lista os dados dos veiculos ")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", content = {
+			@Content(schema = @Schema(implementation = Veiculo.class), mediaType = "application/json") }, description = "Retorna todos os veiculos"),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Não há permissão para acessar o recurso"),
+			@ApiResponse(responseCode = "404", description = "Recurso não encontrado"),
+			@ApiResponse(responseCode = "505", description = "Exceção interna da aplicação") })
 	@GetMapping
-	public List<Veiculo> listar(){
+	public List<Veiculo> listar() {
 		return repository.findAll();
 	}
-	
-	
-	
+
 }
